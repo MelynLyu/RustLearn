4d301b76597f679717badc90f6cdb997cdafb1ae
melynlyu <melynlyu.gmail.com>
2022-07-29 02:04:57 +0800

    Update Guess Game

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Let's play a guess game.");
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("Your guess is {guess}");
 }
